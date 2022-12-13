6d1ae01b7cbabab81bbaa10d281f7cc68dfd185a
“BioBrickDevs” <suomela.jari@gmail.com>
2022-12-14 00:22:23 +0200

    The password manager is now updated to use JSON data and search for passwords and usernames.

--- Day 29 - Password Manager Tkinter/main.py
+++ Day 29 - Password Manager Tkinter/main.py
@@ -3,15 +3,34 @@ import os
 import tkinter.messagebox
 import pyperclip
 import random
-
+import json
 
 
 directory_path = os.getcwd()
 print("My current directory is : " + directory_path)
-filepath ="logo2.gif"
+filepath = "logo2.gif"
 print(filepath)
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+def search():
+    try:
+        with open("data.json", "r") as file:
+            search_term = website_field.get()
+            data = json.load(file)
+            try:
+                print(data[search_term])
+                website = data[search_term]
+
+                username = data[search_term]["email"]
+                password = data[search_term]["password"]
+
+                tkinter.messagebox.askokcancel(
+                    title="Info", message=f"Your credentials for website:{search_term} are\nusername:{username}\npassword:{password}")
+            except:
+                tkinter.Message("not working")
+    except:
+        tkinter.messagebox.showinfo(title="Not found!", message="No data for this website found!")
 
 def generate_password():
 
@@ -41,16 +60,25 @@ def generate_password():
     for char in password_list:
         password += char
 
-    password_field.delete(0,tkinter.END)
-    password_field.insert(0,password)
+    password_field.delete(0, tkinter.END)
+    password_field.insert(0, password)
     pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
 def save_data_to_file():
 
     website = website_field.get()
     email_usr = email_user_field.get()
     password = password_field.get()
+
+    data = {website:
+            {
+                "email": email_usr,
+                "password": password,
+            }
+            }
 
     input = tkinter.messagebox.askokcancel(
         f"Are you sure you want to save this information? {website}, {email_usr}, {password}",
@@ -58,8 +86,18 @@ def save_data_to_file():
 
     if input:
 
-        with open(file="./data.txt", mode="a") as file:
-            file.write(f"{website}|{email_usr}|{password}\n")
+        try:
+            with open(file="data.json", mode="r") as file:
+                save = json.load(file)
+
+            with open(file="data.json", mode="w") as file:
+                save.update(data)
+                print(save)
+                json.dump(save, file)
+        except:
+            with open(file="data.json", mode="w") as file:
+                print("error")
+                json.dump(data, file)
 
         password_field.delete(0, tkinter.END)
         website_field.delete(0, tkinter.END)
@@ -90,19 +128,21 @@ gen_pswd_button = tkinter.Button(
     text="Generate Password", command=generate_password)
 gen_pswd_button.grid(column=2, row=3)
 
-add_button = tkinter.Button(text="Add", width=55, command=save_data_to_file)
+add_button = tkinter.Button(text="Add", width=50, command=save_data_to_file)
 add_button.grid(column=1, row=4, columnspan=2)
+search_button = tkinter.Button(text="Search", command=search)
+search_button.grid(row=1, column=2)
 
-website_field = tkinter.Entry(text="a", width=55)
+website_field = tkinter.Entry(text="a", width=33)
 website_field.insert(0, "")
-website_field.grid(column=1, row=1, columnspan=2)
+website_field.grid(column=1, row=1)
 
 password_field = tkinter.Entry(text="b", width=33)
 password_field.insert(0, "")
 password_field.grid(column=1, row=3, columnspan=1)
 
 
-email_user_field = tkinter.Entry(text="c", width=55)
+email_user_field = tkinter.Entry(text="c", width=53)
 email_user_field.insert(0, "")
 email_user_field.grid(column=1, row=2, columnspan=2)
 
