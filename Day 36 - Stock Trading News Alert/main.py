0f935f2ff554ff20e56696620b465357b852f6c2
“BioBrickDevs” <suomela.jari@gmail.com>
2022-12-22 15:22:22 +0200

    New features

--- Day 36 - Stock Trading News Alert/main.py
+++ Day 36 - Stock Trading News Alert/main.py
@@ -3,49 +3,55 @@ import news
 import send_message
 import dotenv
 import os
-
 dotenv.load_dotenv()
 
 
-STOCK = "MRNA"
-COMPANY_NAME = "Moderna Inc"
-
-
-alpha_vantage_key = os.environ.get("alpha_vantage_key")
-
-parameters = {
-    "apikey": alpha_vantage_key,
-    "function": "TIME_SERIES_DAILY_ADJUSTED",
-    "symbol": STOCK,
-
+under_watch = {
+    "MRNA": "Moderna Inc",
+    "TSLA": "Tesla Inc",
 }
 
 
-response = requests.get("https://www.alphavantage.co/query", params=parameters)
-response.raise_for_status()
-data = response.json()
-data = dict(list(data["Time Series (Daily)"].items())[:2])
+def get_market_values(stock_symbol_alphavantage, company_name_for_new):
 
-data = [float(value["4. close"]) for key, value in data.items()]
+    STOCK = stock_symbol_alphavantage
+    COMPANY_NAME = company_name_for_new
+    alpha_vantage_key = os.environ.get("alpha_vantage_key")
 
-difference = abs(data[1] - data[0])
-average = (data[1] + data[0]) / 2
+    parameters = {
+        "apikey": alpha_vantage_key,
+        "function": "TIME_SERIES_DAILY_ADJUSTED",
+        "symbol": STOCK,
 
-difference_percentage = difference / data[1] * 100
+    }
+    response = requests.get(
+        "https://www.alphavantage.co/query", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    data = dict(list(data["Time Series (Daily)"].items())[:2])
+
+    data = [float(value["4. close"]) for key, value in data.items()]
+
+    difference = abs(data[1] - data[0])
+    difference_percentage = difference / data[1] * 100
+
+    if difference_percentage > 3:
+        # if change over 5 % send message
+
+        message = news.get_latest_news(COMPANY_NAME)
+        stats = ""
+        if data[0] >= data[1]:
+            stats += "+" + str(round(difference_percentage, 2)) + \
+                " %, " + str(round(difference, 2)) + " $" + "<br>"
+        else:
+            stats += "+" + str(round(difference_percentage, 2)) + \
+                " %, " + str(round(difference, 2)) + " $" + "<br>"
+            message = stats + message
+        send_message.send_message(message, COMPANY_NAME)
+        print("Message sent!")
+    print("Script has run!")
 
 
-if difference_percentage > 3:
-    # if change over 5 % send message
-
-    message = news.get_latest_news(COMPANY_NAME)
-    stats = ""
-    round(difference_percentage, 2)
-    round(difference, 2)
-    if data[0] >= data[1]:
-        stats += "+" + str(round(difference_percentage, 2)) + \
-            " %, " + str(round(difference, 2)) + " $" + "<br>"
-    else:
-        stats += "+" + str(round(difference_percentage, 2)) + \
-            " %, " + str(round(difference, 2)) + " $" + "<br>"
-    message = stats + message
-    send_message.send_message(message)
+for (key, value) in under_watch.items():
+    get_market_values(key, value)
+    print(key, value)
--- Day 36 - Stock Trading News Alert/send_message.py
+++ Day 36 - Stock Trading News Alert/send_message.py
@@ -7,10 +7,7 @@ username = os.environ.get("USER_NAME")
 password = os.environ.get("password")
 
 
-
-def send_message(message):
+def send_message(message, subject):
     the_message = message
-   
     yag = yagmail.SMTP(username, password)
-    yag.send(username, "News for Moderna!!", the_message)
-    
+    yag.send(username, subject + " Alert!", the_message)
